7d1f37da2812ce7655000d9a7eadd8033b5458c6
francescooviiedo <francesco.oviedo@gmail.com>
2024-08-27 12:13:10 -0300

    controle

---
+++ src/controle/If.java
@@ -0,0 +1,12 @@
+package controle;
+
+public class If {
+    public static void main(String[] args) {
+        double media = 7.6;
+        if (media >= 7.0) {
+            System.out.println("Aprovado.");
+        } else {
+            System.out.println("Recuperação.");
+        }
+    }
+}
